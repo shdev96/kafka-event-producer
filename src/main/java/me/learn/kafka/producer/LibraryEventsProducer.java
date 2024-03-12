2066dfd6fe0cb74bfd57b13761a74a04c801b541
shdev96 <seunghee.dev@gmail.com>
2024-03-12 23:22:01 +0900

    synchronous event producer

--- src/main/java/me/learn/kafka/producer/LibraryEventsProducer.java
+++ src/main/java/me/learn/kafka/producer/LibraryEventsProducer.java
@@ -45,6 +45,25 @@ public class LibraryEventsProducer {
 			});
 	}
 
+	public SendResult<Integer, String> sendLibraryEventSynchronous(LibraryEvent libraryEvent) throws JsonProcessingException, ExecutionException, InterruptedException, TimeoutException {
+
+		Integer key = libraryEvent.libraryEventId();
+		String value = objectMapper.writeValueAsString(libraryEvent);
+		SendResult<Integer, String> sendResult = null;
+		try {
+			sendResult = kafkaTemplate.sendDefault(key, value).get(1, TimeUnit.SECONDS);
+		} catch (ExecutionException | InterruptedException e) {
+			log.error("ExecutionException/InterruptedException Sending the Message and the exception is {}", e.getMessage());
+			throw e;
+		} catch (Exception e) {
+			log.error("Exception Sending the Message and the exception is {}", e.getMessage());
+			throw e;
+		}
+
+		return sendResult;
+
+	}
+
 	private void handleFailure(Integer key, String value, Throwable ex) {
 		log.error("Error Sending the Message and the exception is {}", ex.getMessage());
 	}
